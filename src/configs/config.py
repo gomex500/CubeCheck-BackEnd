0dec47415f0ffda656d1531fff8136efc85c0470
gomex500 <gomezfreddy886@gmail.com>
2023-09-12 01:18:41 -0600

    configuracion para despliegue

--- src/configs/config.py
+++ src/configs/config.py
@@ -4,4 +4,4 @@ from decouple import config
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 PORT = config('PORT', default=5000, cast=int)
-MONGO_URI = config('MONGO_URI')
+# MONGO_URI = config('MONGO_URI')
